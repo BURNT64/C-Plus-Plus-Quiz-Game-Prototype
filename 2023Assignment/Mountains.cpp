67e7494ad79cb3eda4bcb4d6385071efe2701ec5
BURNT64 <allwood.w2003@gmail.com>
2022-12-06 15:41:04 +0000

    Small update that fixes return errors and other things

--- 2023Assignment/Mountains.cpp
+++ 2023Assignment/Mountains.cpp
@@ -7,10 +7,10 @@ Mountains::Mountains(std::vector<std::string>& filenames)
 
 std::string Mountains::getRandomMountain()
 {
-
+	return " ";
 }
 
 bool Mountains::checkRange(std::string mountain, std::string range)
 {
-
+	return true;
 }
